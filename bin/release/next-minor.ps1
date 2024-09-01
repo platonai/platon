c7f7d5145af3b18121bfcd5a00968ede0d606805
Vincent Zhang <ivincent.zhang@gmail.com>
2024-09-01 21:53:14 +0800

    Imps: 1. improve release scripts

--- bin/release/next-minor.ps1
+++ bin/release/next-minor.ps1
@@ -1,10 +1,12 @@
-# Set up variables
-$bin = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$bin = (Resolve-Path "$bin\..").Path
-$APP_HOME = (Resolve-Path "$bin\..").Path
+# Find the first parent directory containing the VERSION file
+$AppHome=(Get-Item -Path $MyInvocation.MyCommand.Path).Directory
+while ($AppHome -ne $null -and !(Test-Path "$AppHome/VERSION")) {
+  $AppHome=$AppHome.Parent
+}
+cd $AppHome
 
 # Get version information
-$SNAPSHOT_VERSION = Get-Content "$APP_HOME\VERSION" -TotalCount 1
+$SNAPSHOT_VERSION = Get-Content "$AppHome\VERSION" -TotalCount 1
 $VERSION = $SNAPSHOT_VERSION -replace "-SNAPSHOT", ""
 $parts = $VERSION -split "\."
 $PREFIX = $parts[0] + "." + $parts[1]
@@ -18,11 +20,16 @@ $NEXT_SNAPSHOT_VERSION = "$NEXT_VERSION-SNAPSHOT"
 Write-Host "New version: $NEXT_SNAPSHOT_VERSION"
 
 # Update VERSION file
-$NEXT_SNAPSHOT_VERSION | Set-Content "$APP_HOME\VERSION"
+$NEXT_SNAPSHOT_VERSION | Set-Content "$AppHome\VERSION"
 
-# Update $APP_HOME/pom.xml
-$pomXmlPath = "$APP_HOME\pom.xml"
+# Update $AppHome/pom.xml
+$pomXmlPath = "$AppHome\pom.xml"
 ((Get-Content $pomXmlPath) -replace "<tag>v$VERSION</tag>", "<tag>v$NEXT_VERSION</tag>") | Set-Content $pomXmlPath
+
+# Update pom.xml files
+Get-ChildItem "$AppHome" -Depth 3 -Filter 'pom.xml' -Recurse | ForEach-Object {
+  ((Get-Content $_.FullName) -replace $SNAPSHOT_VERSION, $NEXT_SNAPSHOT_VERSION) | Set-Content $_.FullName
+}
 
 # Commit comment
 $COMMENT =$NEXT_SNAPSHOT_VERSION -replace "-SNAPSHOT", ""
